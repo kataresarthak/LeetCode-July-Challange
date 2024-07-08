b89befdd78fce975e4cfec04d252789fb8621eb5
Sarthak R. Katare <kataresarthak001@gmail.com>
2024-07-08 19:38:52 +0530

    Time: 2 ms (53.64%), Space: 7.1 MB (37.53%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int position = 1;
+        int direction = 1;
+        for (int i = 0; i < time; i++) {
+            position += direction;
+            if (position == n || position == 1)
+                direction *= -1;
+        }
+        return position;
+    }
+};
