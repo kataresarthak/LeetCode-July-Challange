1c16bf73c49031c57423f421a920857eae52526b
Sarthak R. Katare <kataresarthak001@gmail.com>
2024-07-25 23:05:06 +0530

    Time: 79 ms (93.50%), Space: 70.9 MB (66.23%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        if (nums.empty()) return nums;
+        
+        int min = *min_element(nums.begin(), nums.end());
+        int max = *max_element(nums.begin(), nums.end());
+        
+        vector<int> count(max - min + 1, 0);
+        for (int num : nums) {
+            count[num - min]++;
+        }
+        
+        int index = 0;
+        for (int i = 0; i < count.size(); i++) {
+            while (count[i]-- > 0) {
+                nums[index++] = i + min;
+            }
+        }
+        return nums;
+    }
+};
